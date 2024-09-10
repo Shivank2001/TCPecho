9dd5e1361d2562482f44d303ffad2af99a5d14d6
Shivank2001 <81311267+Shivank2001@users.noreply.github.com>
2024-09-10 00:51:01 -0500

    Add files via upload

--- client_v1.c
+++ client_v1.c
@@ -9,8 +9,8 @@
 #include<unistd.h>     //to close the connection, fork
 #include<errno.h>      //to check the error number
 #include<netinet/in.h>
-#define BUFFER_SIZE 20
-#define PORT 8080
+#define BUFFER_SIZE 80
+//#define PORT 8080
 
 ssize_t bytesread;
 ssize_t byteswritten;
@@ -18,13 +18,14 @@ ssize_t byteswritten;
 
 
 
-void error_sys();
+void error_sys(const char *message);
 ssize_t writen(int socket1);
+ssize_t readline(int socket1);
 
 char bufferi[BUFFER_SIZE];
 char buffero[BUFFER_SIZE];
  
-int main(){
+int main(int argc, char* argv[]){
     int socket1;
     struct sockaddr_in serveraddr;
     socket1= socket(AF_INET,SOCK_STREAM,0);//AF_INET implies IPV4 protocol, SOCK_STREAM implies TCP connection, 0 implies Internet Protocol
@@ -33,19 +34,28 @@ int main(){
         error_sys("Socket creation failed");
     }
     else{
-        printf("Socket created");
+        printf("Socket created\n");
+    }
+
+    if(argc<3){
+        printf("Enter a valid address");
+        return 0;
     }
     
     serveraddr.sin_family=AF_INET;
-    serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-    serveraddr.sin_port=htons(PORT);
+    serveraddr.sin_port=htons(atoi(argv[2]));
+    serveraddr.sin_addr.s_addr=inet_addr(argv[1]);
 
     if(connect(socket1,(struct sockaddr*)&serveraddr,sizeof(serveraddr))<0){
         error_sys("Connection failed");
     }
     
+    printf("Connected to server\n");
+    
+    
     writen(socket1);
-    printf("Number of bytes written are:%zd\n",byteswritten);
+    //printf("Number of bytes written are:%zd\n",byteswritten);
+    //readline(socket1);
 }
 
 ssize_t writen(int socket1){
@@ -54,7 +64,7 @@ ssize_t writen(int socket1){
             bzero(bufferi, sizeof(bufferi));
             if(fgets(bufferi,sizeof(bufferi),stdin)){
             byteswritten=write(socket1,bufferi,strlen(bufferi));
-            if(byteswritten<0){
+           /* if(byteswritten<0){
                 if(errno==EINTR){
                     goto transmit;
                 }else{
@@ -64,7 +74,17 @@ ssize_t writen(int socket1){
                 return -1;
             }
             return byteswritten;
-            
+            */
+            bytesread= read(socket1,buffero,sizeof(buffero));
+           /* buffero[bytesread] = '\0';
+                
+            if (bytesread<0 && errno ==EINTR)
+            {
+                
+            }*/
+            //return bytesread;
+            fputs(buffero,stdout);
+            //bzero(buffero,sizeof(buffero));
             }
     
         
@@ -73,15 +93,19 @@ ssize_t writen(int socket1){
 
     }
 
-void readline(int socket1){
-            bytesread= read(socket1,buffero,sizeof(buffero));
-            buffero[bytesread] = '\0';
+ssize_t readline(int socket1){
+            for(;;){
+                bytesread= read(socket1,buffero,sizeof(buffero));
+           /* buffero[bytesread] = '\0';
                 
             if (bytesread<0 && errno ==EINTR)
             {
                 
-            }
-            fputs(buffero,stdout);
+            }*/
+            return bytesread;
+            //fputs(buffero,stdout);
+            //bzero(buffero,sizeof(buffero));
+}
 }
 
 void error_sys(const char *message){
